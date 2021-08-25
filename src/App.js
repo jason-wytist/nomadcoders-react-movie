5b6c3d38089f6037b55720369d882dac0433aafd
Jason, wytist <json.ha@gmail.com>
2021-08-25 19:54:56 +0900

    #3.2 done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,17 @@ class App extends React.Component {
   minus = () => {
     this.setState(current => ({count: current.count - 1}));
   };
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye"); 
+  }
   render() {
+    console.log("Im rendering");
     return <div>
       <h1>The number is : {this.state.count}</h1>
       <button onClick={this.add}>Add</button>
